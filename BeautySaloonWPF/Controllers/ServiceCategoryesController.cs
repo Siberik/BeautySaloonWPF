e75aacb0df66990442619c87b1898e0d30dc44a8
user25 <user25@user25.ru>
2022-11-23 16:46:30 +0500

    Сделана связь с API

---
+++ BeautySaloonWPF/Controllers/ServiceCategoryesController.cs
@@ -0,0 +1,29 @@
+﻿using BeautySaloonWPF.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeautySaloonWPF.Controllers
+{
+   public class ServiceCategoryesController
+    {
+        public static List<ServiceCategoryes> GetServiceCategoryes()
+
+        {
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = client.GetAsync($"{Manager.RootUrl}ServiceCategoryes").Result;
+                var content = response.Content.ReadAsStringAsync();
+                var answer = JsonConvert.DeserializeObject<List<ServiceCategoryes>>(content.Result);
+                return answer;
+
+            }
+
+        }
+    }
+}
--- BeautySaloonWPF/MainWindow.xaml.cs
+++ BeautySaloonWPF/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BeautySaloonWPF.Controllers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,7 @@ namespace BeautySaloonWPF
         public MainWindow()
         {
             InitializeComponent();
+            MainListView.ItemsSource = ServiceCategoryesController.GetServiceCategoryes();
         }
     }
 }
