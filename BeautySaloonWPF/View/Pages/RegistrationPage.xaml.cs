b03a32183ada555f33335eafc425008edfe59fba
user25 <user25@user25.ru>
2022-12-07 16:27:05 +0500

    Сделана первичная версия капчи

--- BeautySaloonWPF/View/Pages/RegistrationPage.xaml.cs
+++ BeautySaloonWPF/View/Pages/RegistrationPage.xaml.cs
@@ -25,11 +25,24 @@ namespace BeautySaloonWPF.View.Pages
         public RegistrationPage()
         {
             InitializeComponent();
+            Random rnd = new Random();
+            CaptchaTextBlock.Text = rnd.Next(1000, 9999).ToString();
+            CaptchaTextBlock.LayoutTransform = new RotateTransform(rnd.Next(0,90));
+            CaptchaTextBlock.FontSize = rnd.Next(14,30);
+            CaptchaTextBlock.Width = rnd.Next(50,100);
+            CaptchaTextBlock.Height = rnd.Next(40, 100);
+           CaptchaTextBlock.Foreground= new SolidColorBrush(Color.FromRgb((byte)rnd.Next(1, 255),
+                  (byte)rnd.Next(1, 255), (byte)rnd.Next(1, 233)));
         }
+
 
         private void RegistrationButtonClick(object sender, RoutedEventArgs e)
         {
             
+                
+
+        if(CaptchaTextBlock.Text== WriteCapthaTextBox.Text)
+            {
                 Users newUser = new Users
                 {
                     IdRole = 2,
@@ -45,6 +58,29 @@ namespace BeautySaloonWPF.View.Pages
             MessageBox.Show("Вы успешно зарегестрировались");
             }
             UsersController.AddUser(newUser);
+            }
+            else
+            {
+                MessageBox.Show("Ты ввёл неверную капчу");
+            }
+        }
+
+        private void CaptchaButtonClick(object sender, RoutedEventArgs e)
+        {
+            Random rnd = new Random();
+            CaptchaTextBlock.Text = rnd.Next(1000, 9999).ToString();
+            
+         
+            CaptchaTextBlock.LayoutTransform = new RotateTransform(rnd.Next(0, 90));
+            CaptchaTextBlock.FontSize = rnd.Next(14, 30);
+            CaptchaTextBlock.Width = rnd.Next(50, 100);
+            CaptchaTextBlock.Height = rnd.Next(40, 100);
+            CaptchaTextBlock.Opacity = rnd.Next(50, 90);
+            CaptchaTextBlock.Foreground = new SolidColorBrush(Color.FromRgb((byte)rnd.Next(1, 255),
+                 (byte)rnd.Next(1, 255), (byte)rnd.Next(1, 233)));
+            
+            
+
         }
     }
 }
