f7562b6085311a0b5a806ca0356f27f20251f397
user25 <user25@user25.ru>
2022-12-08 09:53:58 +0500

    Сделан первичный вывод категорий

---
+++ BeautySaloonWPF/Controllers/ServicesController.cs
@@ -0,0 +1,35 @@
+﻿using BeautySaloonWPF.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Services.Description;
+
+namespace BeautySaloonWPF.Controllers
+{
+   public class ServicesController
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+      
+        public static List<Services> GetService()
+
+        {
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = client.GetAsync($"{Manager.RootUrl}Services").Result;
+                var content = response.Content.ReadAsStringAsync();
+                var answer = JsonConvert.DeserializeObject<List<Services>>(content.Result);
+                return answer;
+
+            }
+
+        }
+    }
+}
---
+++ BeautySaloonWPF/Models/Services.cs
@@ -0,0 +1,48 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeautySaloonWPF.Models
+{
+   public class Services
+    {
+
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        [JsonProperty("cost")]
+        public float Cost { get; set; }
+        
+        [JsonProperty("durationInSeconds")]
+        public int DurationInSeconds { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        [JsonProperty("discount")]
+        public float Discount { get; set; }
+
+        [JsonProperty("mainImagePath")]
+        public string MainImagePath { get; set; }
+
+        [JsonProperty("categoryId")]
+        public int CategoryId { get; set; }
+
+        [JsonProperty("category")]
+        public object Category { get; set; }
+
+        [JsonProperty("clientServices")]
+        public object[] ClientServices { get; set; }
+
+        [JsonProperty("servicePhotos")]
+        public object[] ServicePhotos { get; set; }
+       
+
+    }
+}
